3bcac080948bae96518bb325696845ba2dd24cee
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2024-04-07 20:44:54 +0300

    arranged folders

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+
+// import css from './App.module.css'
+// import contacts from '../../data/contacts.json'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+    </div>
+  )
+}
+
+export default App

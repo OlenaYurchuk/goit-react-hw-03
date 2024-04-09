beb1581dd2bed2c21cd38da6511ec1b200ec07c3
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2024-04-09 13:00:26 +0300

    added filter for phonebook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,23 @@
 import SearchBox from '../SearchBox/SearchBox'
 import ContactList from '../ContactList/ContactList'
 // import css from './App.module.css'
-import contacts from '../../data/contacts.json'
+import initialContacts from '../../data/contacts.json'
+import { useState } from 'react'
 
 function App() {
+  const [contacts, setContacts] = useState(initialContacts)
+  const [searchItem, setSearchItem] = useState('');
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchItem.toLocaleLowerCase())
+  );
 
   return (
     <div>
       <h1>Phonebook</h1>
       {/* <ContactForm /> */}
-      <SearchBox />
-      <ContactList contacts={contacts} />
+      <SearchBox value={searchItem} onSearch={setSearchItem} />
+      <ContactList contacts={visibleContacts} />
     </div>
   )
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,13 @@
 import css from './SearchBox.module.css'
 
-export default function SearchBox() {
+export default function SearchBox({ value, onSearch }) {
   return (
     <div className={css.wrap}>
       <p>Find contacts by name</p>
-      <input type="text" className={css.input} />
+      <input type="text"
+        className={css.input}
+        value={value}
+        onChange={(event) => onSearch(event.target.value)} />
     </div>
   )
 }

51709c0cee9ec46345effaff15743ecb6bc53bfe
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2024-04-10 23:40:39 +0300

    saving data to localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,16 @@ import ContactForm from '../ContactForm/ContactForm'
 import SearchBox from '../SearchBox/SearchBox'
 import ContactList from '../ContactList/ContactList'
 import initialContacts from '../../data/contacts.json'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [contacts, setContacts] = useState(initialContacts)
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("saved-contacts");
+    if (savedContacts !== null) {
+      return JSON.parse(savedContacts)
+    }
+    return initialContacts
+  })
   const [searchItem, setSearchItem] = useState('');
 
   const visibleContacts = contacts.filter((contact) =>
@@ -23,6 +29,11 @@ function App() {
       return prevContacts.filter(contact => contact.id !== contactId)
     })
   }
+
+  useEffect(() => {
+    localStorage.setItem("saved-contacts", JSON.stringify(contacts))
+  }, [contacts])
+
   return (
     <div>
       <h1>Phonebook</h1>
